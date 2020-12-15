f7feeef4b1a8ef73c99660f50794ba876f2072e7
Cindy Lau <cindy.lau@shopify.com>
2020-12-14 22:26:58 -0500

    add components for micropost homepage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/Header'
+import MicropostsList from './components/MicropostsList'
+import ProfileCard from './components/ProfileCard';
+import Sidebar from './components/Sidebar';
 
 function App() {
+  const microposts = [
+    {id: 1, content: "This is a test", user: "Jane Doe", created_at: "Dec 12, 2020", followers: 21},
+    {id: 2, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vehicula sed magna ut sodales.", user: "John Smith", created_at: "Dec 15, 2020", followers: 30}
+  ]
+  const latestMicropostsFirst = microposts.reverse();
+
+  const user = {name: "Cindy Lau", followers: 25, following: 35, microposts: 20}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+
+      <div className="app">
+        <Sidebar>
+          <ProfileCard user={user} />
+          <p className="sidebar__text">Placeholder for other content (for example, 'about me', categories, popular posts, social media links, etc)</p>
+        </Sidebar>
+        <MicropostsList microposts={latestMicropostsFirst} />
+      </div>
     </div>
   );
 }
