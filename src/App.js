d8c306af3bcb552e029b9296d1c9f416bfaf75f1
Cindy Lau <cindy.lau@shopify.com>
2020-12-21 15:31:43 -0500

    install and setup polaris

--- src/App.js
+++ src/App.js
@@ -2,26 +2,33 @@ import './App.scss';
 import React from "react";
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import { Header, Footer, About, Contact, Home, Profile, Settings, Users } from './components'
+import '@shopify/polaris/dist/styles.css';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import {AppProvider} from '@shopify/polaris';
+
 
 function App() {
   return (
     <Router>
-      <div>
-        <Header />
+      <AppProvider i18n={enTranslations}>
+        <div>
+          <Header />
 
-        <div className="app">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/profile" component={Profile} />
-            <Route path="/settings" component={Settings} />
-            <Route path="/users" component={Users} />
-          </Switch>
+          <div className="app">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/about" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/polaris" component={Home} />
+              <Route path="/profile" component={Profile} />
+              <Route path="/settings" component={Settings} />
+              <Route path="/users" component={Users} />
+            </Switch>
+          </div>
+
+          <Footer />
         </div>
-
-        <Footer />
-      </div>
+      </AppProvider>
     </Router>
   );
 }
